c800359bf896b1383df560cd15ae796ce9346732
al3xandra1 <92952329+al3xandra1@users.noreply.github.com>
2025-01-12 15:39:06 +0200

    Delete spdemo directory

--- spdemo/src/main/java/gr/hua/dit/spdemo/Models/RegistrationStatus.java
+++
@@ -1,8 +0,0 @@
-package gr.hua.dit.spdemo.Models;
-
-// Καταστάσεις για την εγγραφή
-public enum RegistrationStatus {
-    PENDING,    // Υπό εξέταση
-    APPROVED,   // Εγκρίθηκε
-    REJECTED    // Απορρίφθηκε
-}
