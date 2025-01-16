fb0913963678f378aff8642dc8830b1891fbb797
al3xandra1 <92952329+al3xandra1@users.noreply.github.com>
2025-01-16 18:48:58 +0200

    Add files via upload

---
+++ DsLabApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.ds.ds_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsLabApplication.class, args);
+	}
+
+}
